ecf031a80a9ddf912b5c411dc0ea61abc256590f
govindlohar <govindlohar3210@gmail.com>
2024-12-23 17:40:18 +0530

    p

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ import sendEmail from "./utils/emailSend/sendEmail.js";
 dotenv.config();
 const app = express();  //it return new Express() instace that have many properties and function
 const PORT = process.env.PORT || 8000;
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Define __dirname manually
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());//to allow client side to access 
 app.use(express.static(path.join(__dirname, "build")));
